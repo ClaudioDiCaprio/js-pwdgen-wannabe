fe3647c184c0fd7ad4ff489667491207a69ec043
Claudio <claudiodicaprio89@gmail.com>
2021-11-04 15:47:57 +0100

    done_

--- js/myscript.js
+++ js/myscript.js
@@ -1,1 +1,9 @@
-alert ('hello World!');
+// alert ('hello World!');
+const firstName = prompt('what is your name?');
+const lastName = prompt('what is your surname ?');
+const favColor = prompt('what is your favourite color');
+const password = firstName + lastName + favColor + '21';
+
+console.log(password);
+
+document.getElementById('password').innerHTML = 'your password is: ' + password;
